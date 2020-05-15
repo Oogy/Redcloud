b9a10ef3721e61c343ac48d8ef349b6a9b190472
oogy <tylerweldon94@gmail.com>
2020-05-15 15:38:30 -0400

    reference specific instance

--- redcloud-dns.tf
+++ redcloud-dns.tf
@@ -10,7 +10,7 @@ data "vultr_dns_domain" "redcloud_domain" {
 }
 
 resource "vultr_dns_record" "my_record" {
-  domain = var.redcloud_domain_exists ? data.vultr_dns_domain.redcloud_domain.id : vultr_dns_domain.redcloud_domain.id
+  domain = var.redcloud_domain_exists ? data.vultr_dns_domain.redcloud_domain[0].id : vultr_dns_domain.redcloud_domain[0].id
   name   = "redcloud"
   data   = vultr_server.server[0].main_ip
   type   = "A"
